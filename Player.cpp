9a179eed44d1c63a0d8c2d3ece4bafd69966d960
Tailon <renewedT2016@gmail.com>
2020-02-13 22:38:27 -0900

    Add a comment about default constructor

--- Player.cpp
+++ Player.cpp
@@ -4,6 +4,8 @@
 //This will be the computer
 Player::Player()
 {
+	//This code creates 5 ships that will be given random coordinate points
+	//	then adds them to the vector of ships
 	srand(time(NULL));
 	Ship newShip(std::rand() % 1, std::rand() % 1); //change both "1" to the size of grid x-axis and the y-axis once we've decided what the size is
 	Ship newShip2(std::rand() % 1, std::rand() % 1); //change both "1" to the size of grid x-axis and the y-axis once we've decided what the size is
