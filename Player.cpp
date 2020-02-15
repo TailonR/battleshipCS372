d4ec9c9042a71578443e701dc343aba69f5906a5
swu4 <swu4@alaska.edu>
2020-02-14 15:46:19 -0900

    newShip to newShip1

--- Player.cpp
+++ Player.cpp
@@ -7,12 +7,12 @@ Player::Player()
 	//This code creates 5 ships that will be given random coordinate points
 	//	then adds them to the vector of ships
 	srand(time(NULL));
-	Ship newShip(std::rand() % 1, std::rand() % 1); //change both "1" to the size of grid x-axis and the y-axis once we've decided what the size is
+	Ship newShip1(std::rand() % 1, std::rand() % 1); //change both "1" to the size of grid x-axis and the y-axis once we've decided what the size is
 	Ship newShip2(std::rand() % 1, std::rand() % 1); //change both "1" to the size of grid x-axis and the y-axis once we've decided what the size is
 	Ship newShip3(std::rand() % 1, std::rand() % 1); //change both "1" to the size of grid x-axis and the y-axis once we've decided what the size is
 	Ship newShip4(std::rand() % 1, std::rand() % 1); //change both "1" to the size of grid x-axis and the y-axis once we've decided what the size is
 	Ship newShip5(std::rand() % 1, std::rand() % 1); //change both "1" to the size of grid x-axis and the y-axis once we've decided what the size is
-	ships.push_back(newShip);
+	ships.push_back(newShip1);
 	ships.push_back(newShip2);
 	ships.push_back(newShip3);
 	ships.push_back(newShip4);
